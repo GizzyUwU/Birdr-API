807d6d1ae149b2e2db9db233638106e7889633bb
GizzyUwU <nerop455@gmail.com>
2024-12-02 20:52:18 +0000

    Update index.ts

--- index.ts
+++ index.ts
@@ -2,7 +2,7 @@ import { Client } from "./lib/apiRequests";
 const client = new Client();
 
 const test1 = await client.login(
-  "qYOoV+DUkwcfE2OC0N5ysqmIp0t8pokHI0kvfT65N8BKmXri+kXq2iT5d4OVT7Dx",
+  "TOKEN",
 );
 const test2 = await client.sendMessage("Test!");
 const test3 = await client.logout();
